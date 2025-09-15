7183f292e76b5bbcb5adb10ef86b6c0c86b08301
Denis Panfilov <123986275+DenisPanfilov0@users.noreply.github.com>
2025-09-15 14:38:41 +0300

    Bug Fixed

--- Assets/Code/Gameplay/Behaviour/SpawnZone.cs
+++ Assets/Code/Gameplay/Behaviour/SpawnZone.cs
@@ -9,6 +9,8 @@ namespace Code.Gameplay.Behaviour
         private BoxCollider2D _collider;
         private Camera _camera;
 
+        private const float ReferenceWidth = 1179f; // Фиксированная референсная ширина в пикселях (ваш телефон)
+
         [Inject]
         public void Construct(ItemSpawnerService itemSpawner, Camera mainCamera)
         {
@@ -20,7 +22,26 @@ namespace Code.Gameplay.Behaviour
         {
             _collider = gameObject.GetComponent<BoxCollider2D>();
             _collider.isTrigger = true;
+            AdjustCameraSize();
             UpdateColliderSize();
+        }
+
+        private void AdjustCameraSize()
+        {
+            // Берем референсный orthographicSize с текущей камеры (из инспектора)
+            float referenceOrthographicSize = _camera.orthographicSize;
+
+            // Текущая ширина экрана в пикселях
+            float currentWidth = Screen.width;
+
+            // Корректирующий фактор для достижения 34 при 2028
+            float correctionFactor = 1.35f;
+
+            // Корректируем orthographicSize с учетом ширины и коэффициента
+            float targetOrthographicSize = referenceOrthographicSize * (ReferenceWidth / currentWidth) * correctionFactor;
+
+            // Применяем с ограничением диапазона
+            _camera.orthographicSize = Mathf.Clamp(targetOrthographicSize, 20f, 50f);
         }
 
         private void UpdateColliderSize()
